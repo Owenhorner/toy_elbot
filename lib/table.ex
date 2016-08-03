04dc80620d5d0c4261e4e458db6b1e9b5eec12cb
Owen Horner <owenhorner@live.com.au>
2016-08-04 09:07:40 +1000

    adds table module

---
+++ lib/table.ex
@@ -0,0 +1,8 @@
+defmodule Table do
+  def size(min_width \\ 0, max_width \\ 5, min_length \\ 0, max_length \\ 5) do
+    %{min_width: min_width,
+     max_width: max_width,
+     min_length: min_length,
+     max_length: max_length}
+  end
+end
