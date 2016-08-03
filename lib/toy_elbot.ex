81dd2fb2bd03053bcd44839bd89d00d4da872ee9
Owen Horner <owenhorner@live.com.au>
2016-08-04 09:06:20 +1000

    moved main function for escript to toy_elbot.ex

--- lib/toy_elbot.ex
+++ lib/toy_elbot.ex
@@ -1,2 +1,12 @@
 defmodule ToyElbot do
+  alias ToyElbot.Interface
+
+  def main(_args) do
+    initialize_elbot
+    Interface.start
+  end
+
+  defp initialize_elbot do
+    {:ok, _} = GenServer.start_link(ToyElbot.ControlSystem, [], name: :elbot)
+  end
 end
